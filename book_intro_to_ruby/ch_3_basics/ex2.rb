f6ef83dd4c03f93e5c7dc109a66203265c54682e
Ginni Pinckert <gcpinckert@gmail.com>
2021-01-14 16:56:13 -0800

    Add ch. 3, basics: ex 1-4

---
+++ book_intro_to_ruby/ch_3_basics/ex2.rb
@@ -0,0 +1,11 @@
+num = 5346
+thousands_place = 5346 / 1000
+hundreds_place = (5346 / 100) % 10
+tens_place = (5346 % 100) / 10
+ones_place = 5346 % 10
+
+puts num
+puts thousands_place
+puts hundreds_place
+puts tens_place
+puts ones_place
